d4f7c84cc15950fdff94c44601f1c711428d7249
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-25 14:56:27 +0300

    Initial Commit

---
+++ TestParsingJson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestParsingJson
+//
+//  Created by Алексей Гуляев on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
